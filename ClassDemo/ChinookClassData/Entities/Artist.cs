361632e023a731e789fd142a82554e658b5b8fdb
jadanza1 <43007037+jadanza1@users.noreply.github.com>
2019-05-06 21:46:29 -0600

    Added Entity ARtist Class
    Forgot to save

--- ClassDemo/ChinookClassData/Entities/Artist.cs
+++ ClassDemo/ChinookClassData/Entities/Artist.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+#endregion
+
 namespace ChinookClassData.Entities
 {
-    class Artist
+    //default class permisison is private
+    //annotation to link this class to the sql table
+    [Table("Artists")]
+    public class Artist
     {
+        //[Key DatabaseGenerated(DatabaseGeneratedOption.None)]
+        //[Key, Column(Order = )]
+
+        private string _Name;
+
+        [Key]
+        public int ArtistId { get; set; }
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+        }
+
+
+
     }
 }
