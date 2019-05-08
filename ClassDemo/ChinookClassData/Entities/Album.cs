40843d727dc9f395c92e435fb5a35dc00ec53bd5
jadanza1 <43007037+jadanza1@users.noreply.github.com>
2019-05-08 15:28:56 -0600

    Added Album Class

---
+++ ClassDemo/ChinookClassData/Entities/Album.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+
+namespace ChinookClassData.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        private string _ReleaseLabel;
+
+        [Key]
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    _ReleaseLabel = null;
+                }
+                else
+                {
+                    _ReleaseLabel = value;
+                }
+            }
+        }
+    }
+}
