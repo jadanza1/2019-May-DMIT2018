341adbff82ce02a88cc7490462fdf90e5cb6049d
jadanza1 <43007037+jadanza1@users.noreply.github.com>
2019-05-08 21:42:16 -0600

    Added Album Controller and Artist Controller

---
+++ ClassDemo/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.DAL;
+using ChinookSystem.Data.Entities;
+using System.ComponentModel;
+#endregion
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Album> Album_List()
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                return context.Albums.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Album Album_Get(int albumId)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                return context.Albums.Find(albumId);
+            }
+        }
+    }
+}
