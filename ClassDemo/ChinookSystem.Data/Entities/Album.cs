fa4786d22a179f08482a4a55d3b4ca8d62512314
jadanza1 <43007037+jadanza1@users.noreply.github.com>
2019-05-08 20:13:54 -0600

    Fixed ChinookSystem Data Folder

---
+++ ClassDemo/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        private string _ReleaseLabel;
+
+        [Key]
+        public int AlbumId { get; set; }
+        [Required(ErrorMessage = "Title Is Required")]
+        [StringLength(160, ErrorMessage = "Title is Limited To 160 characters")]
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        [Range(1950, 2019, ErrorMessage = "Release Year Must be Between 1950 and Today")] // will be fixed. should be 1950 - Datetime.Today.Now
+        public int ReleaseYear { get; set; }
+        [StringLength(50, ErrorMessage = "Release Label Limited to 50 characters")]
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _ReleaseLabel = null;
+                }
+                else
+                {
+                    _ReleaseLabel = value;
+                }
+            }
+        }
+    }
+}
