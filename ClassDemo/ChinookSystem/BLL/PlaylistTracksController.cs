2335f210ad2dded90d42ae21106b452d5708d4d7
jadanza1 <43007037+jadanza1@users.noreply.github.com>
2019-06-03 21:41:27 -0600

    Started Add Song to Tracks Transaction

--- ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
+++ ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
@@ -10,6 +10,7 @@ using ChinookSystem.Data.DTOs;
 using ChinookSystem.Data.POCOs;
 using ChinookSystem.DAL;
 using System.ComponentModel;
+using DMIT2018Common.UserControls;
 #endregion
 
 namespace ChinookSystem.BLL
@@ -43,7 +44,66 @@ namespace ChinookSystem.BLL
             using (var context = new ChinookSystemContext())
             {
                 //code to go here
-                
+
+                //there are two default data types from linq given to 
+                // the datatype var : IEnumerable or IQueryable
+
+                Playlist exists = (from x in context.Playlists
+                                     where x.Name.Equals(playlistname) && x.UserName.Equals(username)
+                                     select x).FirstOrDefault();
+                PlaylistTrack newtrack = null;
+                // this list is required for  use by the BusinessRuleException of the MessageUserControl
+                List<string> reasons = new List<string>(); 
+
+                int tracknumber = 0;
+                //Determine if the playlist ("parent") instance needs to be created.
+                if (exists == null)
+                {
+                    //create a new playlist
+                    exists.Name = playlistname;
+                    exists.UserName = username;
+                    // the .Add(item) ONLY stages your record for adding to the database
+                    // the actual phyiscal add to the database in done on .SaveChanges()
+                    // the return data instance from the Add will happen when 
+                    // the .SaveChanges() is actually executed.
+                    // thus there is a logic delay on this code.
+                    // there is no real IDENTITY in the returned instance
+                    // IF YOU ACCESS THE INSTANCE YOUR PKEY VALUE WILL BE 0.
+                    exists = context.Playlists.Add(exists);
+
+                    //since this is a new playlist, logically the tracknumber will be 1.
+                    tracknumber = 1;
+                    
+                }
+                else
+                {
+                    // Calculate the next track number for an existing playlist.
+                    tracknumber = exists.PlaylistTracks.Count() + 1;
+
+                    //RESTRICTION (BusinessRule) 
+                    //BusinessRule Requires All to be in a single List<string>
+                    // A Track may only exists on a playlist once.
+
+                    newtrack = exists.PlaylistTracks.SingleOrDefault(x => x.TrackId == trackid);
+                    if(newtrack != null)
+                    {
+                        reasons.Add("Track Already Exists on playlist.");
+                    }
+                    
+                    //do we add the track to the playlist
+                    if(reasons.Count() > 0 )
+                    {
+                        //some business rule within the transaction has failed.
+                        //throw the business rule exception.
+                        throw new BusinessRuleException("Adding Track to Playlist.", reasons);
+                    }
+                    else
+                    {
+                        //Part Two
+                        //adding the new track to the PlaylistTracks table.
+                    }
+
+                }
              
             }
         }//eom
